536dd2c3a5749628ae196f3b4f94c7895f34a7f2
Tye Blackie <blackie.tye@gmail.com>
2017-08-08 11:14:21 -0700

    Initial Commit

---
+++ Every.Do/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Every.Do
+//
+//  Created by Tye Blackie on 2017-08-08.
+//  Copyright © 2017 Tye Blackie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Every.Do/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  Every.Do
+//
+//  Created by Tye Blackie on 2017-08-08.
+//  Copyright © 2017 Tye Blackie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
